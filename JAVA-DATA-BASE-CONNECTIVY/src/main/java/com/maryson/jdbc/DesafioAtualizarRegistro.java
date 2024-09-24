deae9c7a89937769cda4f8be675f4f4244f88948
maryson gleison <maryson011@gmail.com>
2024-09-24 15:47:30 -0300

    feat: desafio excluir registro (jdbc)

--- JAVA-DATA-BASE-CONNECTIVY/src/main/java/com/maryson/jdbc/DesafioAtualizarRegistro.java
+++ JAVA-DATA-BASE-CONNECTIVY/src/main/java/com/maryson/jdbc/DesafioAtualizarRegistro.java
@@ -9,15 +9,17 @@ import java.util.List;
 import java.util.Scanner;
 
 public class DesafioAtualizarRegistro {
+    private static Connection conn;
+    private static String sqlPesquisa = "SELECT * FROM pessoas WHERE nome LIKE ?";
+    private static String sqlAtualiza = "UPDATE pessoas SET nome = ? WHERE codigo = ?";
+    private static String sqlDeleta = "DELETE from pessoas WHERE codigo = ?";
+
     public static void main(String[] args) throws SQLException {
 
         Scanner entrada = new Scanner(System.in);
         System.out.println("Digite o parâmetro para pesquisa: ");
         String parametro = entrada.nextLine();
 
-        String sqlPesquisa = "SELECT * FROM pessoas WHERE nome LIKE ?";
-        String sqlAtualiza = "UPDATE pessoas SET nome = ? WHERE codigo = ?";
-        String sqlDeleta = "DELETE pessoas WHERE codigo = ?";
         Connection conn = FabricaConexao.getConexao();
 
         PreparedStatement stmtSCH = conn.prepareStatement(sqlPesquisa);
@@ -31,25 +33,61 @@ public class DesafioAtualizarRegistro {
             data.add(new Pessoa(id, nome));
         }
 
+        System.out.println("Deseja alterar um registro ou deletar? Digite [atualizar ou Deletar]");
+        String operacao = entrada.nextLine();
+        if ("atualizar".equalsIgnoreCase(operacao)) {
+            atualizarRegistro(conn, data);
+        } else if ("deletar".equalsIgnoreCase(operacao)){
+            deletarRegistro(conn, data);
+        }
+
+        entrada.close();
+        stmtSCH.close();
+        conn.close();
+
+    }
+
+    private static void deletarRegistro(Connection conn, List<Pessoa> data) {
+        System.out.println("Qual registro deseja alterar? Digite o id: ");
+        for(Pessoa d: data) {
+            System.out.println(d.getCodigo() + ": " + d.getNome());
+        }
+        Scanner entrada = new Scanner(System.in);
+        int id = entrada.nextInt();
+        entrada.nextLine();
+
+        try {
+            PreparedStatement stmtDeletar = conn.prepareStatement(sqlDeleta);
+            stmtDeletar.setInt(1, id);
+            stmtDeletar.execute();
+            stmtDeletar.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static void atualizarRegistro(Connection conn, List<Pessoa> data) {
         System.out.println("Digite o cógigo que deseja alterar o nome: ");
         for(Pessoa d: data) {
-            System.out.println(d.getCodigo() + "> " + d.getNome());
+            System.out.println(d.getCodigo() + ": " + d.getNome());
         }
+        Scanner entrada = new Scanner(System.in);
         int id = entrada.nextInt();
         entrada.nextLine();
 
         System.out.println("Digite o novo nome: ");
         String novoNome = entrada.nextLine();
 
-        PreparedStatement stmtAtualizar = conn.prepareStatement(sqlAtualiza);
-        stmtAtualizar.setString(1, novoNome);
-        stmtAtualizar.setInt(2, id);
-        stmtAtualizar.execute();
-
-        entrada.close();
-        stmtSCH.close();
-        stmtAtualizar.close();
-        conn.close();
+        try {
+            PreparedStatement stmtAtualizar = conn.prepareStatement(sqlAtualiza);
+            stmtAtualizar.setString(1, novoNome);
+            stmtAtualizar.setInt(2, id);
+            stmtAtualizar.execute();
+            stmtAtualizar.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
