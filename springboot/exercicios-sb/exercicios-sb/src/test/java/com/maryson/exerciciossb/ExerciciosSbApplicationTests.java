04a68f705c2dedfeceb6c653425d4a6ec7a1afbb
maryson gleison <maryson011@gmail.com>
2024-10-01 17:07:08 -0300

    config: niciando com spring boot

---
+++ springboot/exercicios-sb/exercicios-sb/src/test/java/com/maryson/exerciciossb/ExerciciosSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maryson.exerciciossb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
