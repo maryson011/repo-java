e8af8d180a09f8362950383ac2e214ec604b0b26
maryson gleison <maryson011@gmail.com>
2024-10-03 09:27:10 -0300

    feat: crud basico (spring mvc)

--- springboot/exercicios-mvc/src/main/java/com/maryson/exerciciosmvc/controllers/ProdutoController.java
+++ springboot/exercicios-mvc/src/main/java/com/maryson/exerciciosmvc/controllers/ProdutoController.java
@@ -6,6 +6,8 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
@@ -13,20 +15,43 @@ public class ProdutoController {
     @Autowired // injeção de dependência
     private ProdutoRepository produtoRepository;
 
-    @PostMapping
-    public @ResponseBody Produto novoProduto(
-            @RequestParam String nome,
-            @RequestParam double preco,
-            @RequestParam double desconto
-    ) {
-        Produto produto = new Produto(nome, preco, desconto);
+//    @PostMapping
+//    public @ResponseBody Produto novoProduto(
+//            @RequestParam String nome,
+//            @RequestParam double preco,
+//            @RequestParam double desconto
+//    ) {
+//        Produto produto = new Produto(nome, preco, desconto);
+//        produtoRepository.save(produto);
+//        return produto;
+//    }
+
+//    @PostMapping("/produto2")
+    @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+    public @ResponseBody Produto novoProduto(@Valid Produto produto) {
         produtoRepository.save(produto);
         return produto;
     }
 
-    @PostMapping("/produto2")
-    public @ResponseBody Produto novoProduto2(@Valid Produto produto) {
-        produtoRepository.save(produto);
-        return produto;
+    @GetMapping
+    public Iterable<Produto> obterProdutos() {
+        return produtoRepository.findAll();
     }
+
+    @GetMapping(path = "/{id}")
+    public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
+        return produtoRepository.findById(id);
+    }
+
+//    @PutMapping
+//    public Produto alterarProduto(@Valid Produto produto) {
+//        produtoRepository.save(produto);
+//        return produto;
+//    }
+
+    @DeleteMapping(path = "/{id}")
+    public void excluirProduto(@PathVariable int id) {
+        produtoRepository.deleteById(id);
+    }
+
 }
