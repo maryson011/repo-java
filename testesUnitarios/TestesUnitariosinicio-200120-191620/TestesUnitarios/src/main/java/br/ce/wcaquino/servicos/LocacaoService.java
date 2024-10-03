e32c6befb6d768351e48d8d71f15a1c6fc91d57a
maryson gleison <maryson011@gmail.com>
2024-10-03 13:47:42 -0300

    refactor: separado teste para pasta de testes (junit)

--- testesUnitarios/TestesUnitariosinicio-200120-191620/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ testesUnitarios/TestesUnitariosinicio-200120-191620/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -30,18 +30,4 @@ public class LocacaoService {
 		
 		return locacao;
 	}
-
-	@Test
-	public void teste() {
-		// cenario
-		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Usuario 1");
-		Filme filme = new Filme("Filme 1", 2, 5.4);
-		// ação
-		Locacao locacao = service.alugarFilme(usuario, filme);
-		// verificação
-		Assert.assertTrue(locacao.getValor() == 5.4);
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-	}
 }
---
+++ testesUnitarios/TestesUnitariosinicio-200120-191620/TestesUnitarios/src/test/java/br/ce/wcaquinho/servicos/LocacaoServiceTest.java
@@ -0,0 +1,27 @@
+package br.ce.wcaquinho.servicos;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.servicos.LocacaoService;
+import br.ce.wcaquino.utils.DataUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Date;
+
+public class LocacaoServiceTest {
+    @Test
+    public void teste() {
+        // cenario
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Usuario 1");
+        Filme filme = new Filme("Filme 1", 2, 5.4);
+        // ação
+        Locacao locacao = service.alugarFilme(usuario, filme);
+        // verificação
+        Assert.assertTrue(locacao.getValor() == 5.4);
+        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+    }
+}
