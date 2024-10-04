ca4dc40b9843e2f929e2f427ab6fa033b3da44d8
maryson gleison <maryson011@gmail.com>
2024-10-04 08:21:51 -0300

    feat: error.checkThat (junit)

--- testesUnitarios/TestesUnitariosinicio-200120-191620/TestesUnitarios/src/test/java/br/ce/wcaquinho/servicos/LocacaoServiceTest.java
+++ testesUnitarios/TestesUnitariosinicio-200120-191620/TestesUnitarios/src/test/java/br/ce/wcaquinho/servicos/LocacaoServiceTest.java
@@ -7,21 +7,29 @@ import br.ce.wcaquino.servicos.LocacaoService;
 import br.ce.wcaquino.utils.DataUtils;
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import java.util.Date;
 
 public class LocacaoServiceTest {
+
+    @Rule
+    public ErrorCollector error = new ErrorCollector();
+
     @Test
     public void teste() {
         // cenario
         LocacaoService service = new LocacaoService();
         Usuario usuario = new Usuario("Usuario 1");
         Filme filme = new Filme("Filme 1", 2, 5.4);
+
         // ação
         Locacao locacao = service.alugarFilme(usuario, filme);
+
         // verificação
-        Assert.assertThat(locacao.getValor(), CoreMatchers.is(5.4));
+        error.checkThat(locacao.getValor(), CoreMatchers.is(5.4));
         Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
         Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
     }
